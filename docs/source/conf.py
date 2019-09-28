a1d1d1a1214f5364da53ab05c1c475e485f0421b
Diogo Silva <diogo.silva@devbusters.com>
2019-09-28 19:30:39 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, Diogo Silva'
 author = u'Diogo Silva'
 
 # The short X.Y version
-version = u'0.1'
+version = u'1.0'
 # The full version, including alpha/beta/rc tags
-release = u'0.1.0'
+release = u'1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
